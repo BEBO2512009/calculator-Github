5ffd8a53456b9420c5ddf4956a4017b81cfb8ac0
BEBO2512009 <abanoubromani0@gmail.com>
2025-08-06 21:49:40 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -15,6 +15,18 @@ calculatorContainer.addEventListener("click", e => {
             case "=":
                 evaluate();
                 break;
+            case "x²":
+                squareNumber();
+                break;
+            case "x³":
+                cubeNumber();
+                break;
+            case "√":
+                squareRoot();
+                break; 
+            case "∛":
+                cubeRoot();
+                break; 
             default:
                 addToDisplayarea(e.target.textContent);
         }
@@ -44,4 +56,46 @@ function evaluate() {
     } catch (error) {
        area.textContent = 'ERROR';
     }
+
+ function squareNumber() {
+    try {
+        let num = parseFloat(area.textContent);
+        if (isNaN(num)) throw Error();
+        area.textContent = num ** 2;
+    } catch {
+        area.textContent = "ERROR";
+    }
+}
+
+function squareRoot() {
+    try {
+        let num = parseFloat(area.textContent);
+        if (isNaN(num) || num < 0) throw Error();
+        area.textContent = Math.sqrt(num);
+    } catch {
+        area.textContent = "ERROR";
+    }
+}
+
+function cubeNumber() {
+    try {
+        let num = parseFloat(area.textContent);
+        if (isNaN(num)) throw Error();
+        area.textContent = Math.pow(num, 3); // أو num ** 3
+    } catch {
+        area.textContent = "ERROR";
+    }
+}
+
+function cubeRoot() {
+    try {
+        let num = parseFloat(area.textContent);
+        if (isNaN(num)) throw Error();
+        area.textContent = Math.cbrt(num).toFixed(6); // دقة 6 أرقام
+    } catch {
+        area.textContent = "ERROR";
+    }
+}
+
+
 }
